ff89e3ca5bfc1e1d9620942b7eb766ee9ba40d78
santiago hazana <santiagohazana@gmail.com>
2020-12-15 19:21:47 +0100

    Changes

--- ms-dos/practica1-extra/test.bat
+++ ms-dos/practica1-extra/test.bat
@@ -1,3 +1,11 @@
 @echo off
 
-if exist %1 echo %1
+set /p num="Ingrese un numero: "
+set /p exp="Ingrese el exponente: "
+set res=1
+for /L %%i in ( 1, 1, %exp% ) do (
+    set /a res*=%num% 
+    ::set /a res=%res% * %num% esto por alguna razon no funciona, pero equivale a lo de arriba
+)
+
+echo %res%
