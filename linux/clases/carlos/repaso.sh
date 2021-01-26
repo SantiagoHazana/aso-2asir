51e286e7a3b69eb58ddd2243e4ea6089e8a0ce8d
santiago hazana <santiagohazana@gmail.com>
2021-01-26 19:02:46 +0100

    Clase carlos

---
+++ linux/clases/carlos/repaso.sh
@@ -0,0 +1,108 @@
+# echo Ingrese una palabra; read palabra
+# echo
+# echo $palabra
+# palabra=carlos
+# echo $palabra
+
+
+# echo $1
+# echo $2
+# echo
+# echo $1 $2
+
+# echo Ingrese un numero; read num
+# if [ $num -lt 0 ]
+# then
+#     echo El numero es negativo
+# else
+#     echo El numero es positivo
+# fi
+
+# find /home -name "calc.sh" > /dev/null
+
+# if [ $? -eq 0 ]
+# then
+#     echo Se encontro el fichero
+# else
+#     echo No se encontro el fichero
+# fi
+
+# echo Ingrese una palabra; read palabra
+
+# if [ -f $palabra ]
+# then
+#     echo Es un fichero
+# elif [ -d $palabra ]
+# then
+#     echo Es un directorio
+# else
+#     echo No es nada
+# fi
+
+# echo Bucle While
+
+# num=0
+# while [ $num -lt 10 ]
+# do
+#     echo $num
+#     num=$(($num + 1))
+# done
+
+# echo
+# echo Bucle Until
+
+# num=0
+# until [ $num -ge 10 ]
+# do
+#     echo $num
+#     num=$(($num + 1))
+# done
+
+# echo
+# echo "Bucle foreach"
+# echo Se ingresaron $# parametros
+# for i in $*
+# do
+#     echo $i
+# done
+
+# #bucle foreach que recorre un rango de valores
+# for i in $(seq 0 9)
+# do
+#     echo $i
+# done
+# #bucle foreach que recorre un rango de valores
+# for i in {0..9}
+# do
+#     echo $i
+# done
+
+# echo
+# echo "Bucle for"
+# for ((i=0; i<10; i++)) 
+# do
+#     echo $i
+# done
+
+# ejecutar algo hasta que se indique lo contrario
+
+# resp="y"
+# until [ $resp = "n" -o $resp = "N" -o $resp = "no" -o $resp = "No"]
+# do
+#     echo 
+#     whoami
+#     pwd
+#     echo Desea repetirlo? N=No, Y=Si
+#     read resp
+# done
+
+# Hecho con while
+# resp="y"
+# while [ $resp = "y" -o $resp = "Y" ]
+# do
+#     echo 
+#     whoami
+#     pwd
+#     echo Desea repetirlo? N=No, Y=Si
+#     read resp
+# done
---
+++ linux/examen1/ej1.sh
@@ -0,0 +1,10 @@
+# 1. Realizar un script que realice la lectura de una variable que será introducida por 
+# el usuario al inicio de su ejecución. A continuación preguntará al usuario si 
+# lo introducido es un fichero o de un directorio. Si lo introducido coincide con lo 
+# pedido, es decir, si existe y es lo indicado, saldrá un mensaje indicando tal éxito; 
+# en caso contrario, comprobará si existe donde nos encontramos, y nos indicará el éxito o 
+# el fracaso, y a continuación preguntará si se desea buscar en algún directorio en particular 
+# o en todo el disco dicho valor con tal característica. Si se logra encontrar, nos saldrá 
+# un mensaje de éxito, dando al usuario la posibilidad de mostrarlo por pantalla (hay que 
+# mostrar su contenido de forma correcta); y en caso contrario, nos mostrará el valor introducido 
+# y nos indicará su nula existencia.
