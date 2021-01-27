03480b611d93586565ec03158cb6864d2ee62b4d
Santiago Hazana <santiagohazana@gmail.com>
2021-01-27 13:55:13 +0100

    Added for in potencia

--- linux/clases/Sara/potencia.sh
+++ linux/clases/Sara/potencia.sh
@@ -12,5 +12,18 @@ done
 echo
 echo $resultado
 
-# Se puede hacer directamente de la siguiente manera
-echo $(($num1**$num2))
+# # Se puede hacer directamente de la siguiente manera
+# echo $(($num1**$num2))
+
+# for ((i=0; i<$num2; i++)); do
+#     resultado=$(($resultado*$num1))
+# done
+# echo
+# echo $resultado
+
+# for i in $(seq 1 $num2)
+# do
+#     resultado=$(($resultado*$num1))
+# done
+# echo
+# echo $resultado
