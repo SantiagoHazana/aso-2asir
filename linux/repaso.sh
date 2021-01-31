ce49b893b7be00b8fe36c2b72acea7f542776140
santiago hazana <santiagohazana@gmail.com>
2021-01-31 19:15:15 +0100

    Algo algo

--- linux/repaso.sh
+++ linux/repaso.sh
@@ -1,12 +1,15 @@
-contador=0;
-resp=""
-while [ $contador -lt 3 -a $resp != "n" ]
+for i in $*
 do
-    echo $contador
-    contador=$(($contador + 1))
-    echo
-    echo Desea continuar? [s/n]; read resp
+    if [ -f $i ]
+    then
+        echo Es un fichero $i
+    elif [ -d $i ]
+    then
+        echo Es un directorio $1
+    elif [ "$i" -eq "$i" ] 2>/dev/null
+    then
+        echo Es un numero
+    else
+        echo Es una palabra
+    fi
 done
-
-# contador=0
-# until [ $contador -ge 3 ]
--- linux/test.sh
+++
@@ -1,8 +0,0 @@
-num=`echo $1 % 1 | bc`
-
-if [ $num = 0 ]
-then
-    echo Entero
-else
-    echo Con decimales
-fi
