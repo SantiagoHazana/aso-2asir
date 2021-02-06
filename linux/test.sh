b76af14ecf7380d3a19c2331196dc1d1000ac04a
santiago hazana <santiagohazana@gmail.com>
2021-02-06 16:46:12 +0100

    Test errores

--- linux/test.sh
+++ linux/test.sh
@@ -1,20 +1,7 @@
-echo Ingrese un numero de filas a eliminar de arriba; read num1
-echo Ingrese un numero de filas a eliminar de abajo; read num2
-filas=`wc -l < $1`
-filas=$(($filas - $num1 + 1))
-tail -$filas $1 > temp.txt
-mv temp.txt $1
-
-
-filas=$(($filas-$num2))
-head -$filas $1 > temp.txt
-mv temp.txt $1
-
-
-# echo Ingrese un numero de filas a eliminar de arriba; read num1
-# echo Ingrese un numero de filas a eliminar de abajo; read num2
-# filas=`wc -l < $1`
-# filasSup=$(($filas - $num1 + 1))
-# filasInf=$(($filas - $num2))
-# tail -$filasSup $1 | head -$filasInf > temp.txt
-# mv temp.txt $1
+mv $1 $2 2> /dev/null
+if [ $? -eq 0 ]
+then
+    echo Se movio exitosamente $1 a la carpeta $2
+else
+    echo No se pudo mover, alguno no existe
+fi
